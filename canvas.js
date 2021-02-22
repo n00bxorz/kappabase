fbfa85e123163fea6361569e4e2b2e6636144ad3
n00bxorz <79425161+n00bxorz@users.noreply.github.com>
2021-02-21 18:03:37 -0800

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -42,7 +42,7 @@ class Emote {
     ctx.rotate(((2 * Math.PI) / 60) * time.getSeconds() + ((2 * Math.PI) / 60000) * time.getMilliseconds());
     ctx.translate(105, 0);
     ctx.fillRect(0, -12, 50, 24); // Shadow
-    ctx.drawImage(earth, -12, -12);
+    ctx.drawImage(Emote, -12, -12);
     this.element = element;
     const ratio = element.width / element.height;
     this.height = emoteScale;
